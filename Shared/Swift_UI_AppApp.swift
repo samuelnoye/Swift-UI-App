6ed08c686c0508f9f72040223bb719328ac91ac6
Samuel Noye <samuel.noye@amalitech.com>
2021-12-06 15:55:33 +0000

    Initial Commit

---
+++ Shared/Swift_UI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_AppApp.swift
+//  Shared
+//
+//  Created by Samuel Noye on 06/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
