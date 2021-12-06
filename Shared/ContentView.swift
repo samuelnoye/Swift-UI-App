3b3464b3d0b4d8f3ed9f8a54c152ad1fbe8a6658
Samuel Noye <samuel.noye@amalitech.com>
2021-12-06 16:36:52 +0000

    initial commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,10 +7,34 @@
 
 import SwiftUI
 
+extension Image{
+    func imageModifier()->some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    func iconModifier()-> some View {
+        self
+            .imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
 struct ContentView: View {
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        AsyncImage(url: URL(string: imageURL)){
+            Image in
+            Image.imageModifier()
+                
+        }placeholder:{
+            Image(systemName: "photo.circle.fill")
+                .iconModifier()
+        }
     }
 }
 
