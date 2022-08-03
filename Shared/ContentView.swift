afefd2e0c2ffc2feddf6ceda1b515a754f7e075c
Samuel Noye <samuel.noye@amalitech.org>
2022-08-03 15:26:50 +0000

    Update ContentView.swift

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -17,7 +17,7 @@ extension Image{
         self
             .imageModifier()
             .frame(maxWidth: 128)
-           // .foregroundColor(.purple)
+            .foregroundColor(.purple)
             .opacity(0.5)
     }
 }
